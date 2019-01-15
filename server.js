c10b26c5c451f5dbf29f587d054db74122af29a4
ORTIJ262 <Juan.Pablo.X.Ortiz.Palacio.-ND@disney.com>
2019-01-15 00:21:20 -0500

    Add initial routes for todo API

--- server.js
+++ server.js
@@ -3,9 +3,40 @@ var app = express();
 
 const PORT = process.env.PORT || 3000;
 
+var todoList = [{
+    id: '1',
+    description: 'ToDo example',
+    completed: false
+}, {
+    id: '2',
+    description: 'Second ToDo example',
+    completed: false
+}, {
+    id: '3',
+    description: 'Third ToDo example',
+    completed: true
+}];
+
 app.get('/', function (req, res) {
     res.send('API root');
 });
+
+// GET /todos
+app.get('/todos', function (req, res) {
+    res.json(todoList);
+});
+
+// GET /todos/id
+app.get('/todos/:id', function (req, res) {
+    var result = todoList.find(e => e.id === req.params.id);
+
+    if (result) {
+        res.json(result);
+    } else {
+        res.status(404).send();
+    }
+});
+
 
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + ' ...');
